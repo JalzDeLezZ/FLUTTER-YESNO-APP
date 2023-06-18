29695e669a0681faab871da590468b2504fc7c85
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-06-17 19:20:24 -0500

    [add] input message / END Design

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class MessageFieldBoxScreen extends StatelessWidget {
+  const MessageFieldBoxScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: const BorderSide(color: Colors.transparent),
+    );
+    final inputDecoration = InputDecoration(
+      hintText: 'Type a message',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Send message $textValue');
+          textController.clear();
+        },
+      ),
+    );
+    return TextFormField(
+      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Send message $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+      // onChanged: (value) {
+      //   print('Typing message $value');
+      // },
+    );
+  }
+}
